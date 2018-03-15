e60b0c8f3dfaabd3a3e9d48325f98b4abc232e5e
wzj <jay_wzj@163.com>
2018-03-15 18:04:58 +0800

    home orders 渲染数据

--- src/components/details/orders.vue
+++ src/components/details/orders.vue
@@ -1,44 +1,61 @@
 <template>
   <div class="orders_wrap">
     <router-link to="/details">
-      <div class="orders_wrap_item">
-        <div class="time">取消时间：2017-9-11 12：22：30<span class="cancel">已取消</span></div>
+      <div class="orders_wrap_item" v-for="item of ordersList">
+        <div class="time">{{item.statusDatePage}}<span :class="item.statusClass">{{item.statusPage}}</span></div>
         <div class="content">
-          <img src="@/assets/2.jpg" alt="" class="pic">
+          <img :src="item.orderPic.picUrl" alt="" class="pic">
           <div>
-            <div class="name">商品名称</div>
-            <div class="price">预估价格：<span>￥39.9</span></div>
+            <div class="name">{{item.category.name}}</div>
+            <div class="price">预估价格：<span>￥{{item.price}}</span></div>
           </div>
         </div>
       </div>
     </router-link>
-    <div class="orders_wrap_item">
-      <div class="time">取消时间：2017-9-11 12：22：30<span class="cancel">平台已取消</span></div>
-      <div class="content">
-        <img src="@/assets/2.jpg" alt="" class="pic">
-         <div>
-          <div class="name">商品名称</div>
-          <div class="price">预估价格：<span>￥39.9</span></div>
-        </div>
-      </div>
-    </div>
-    <div class="orders_wrap_item">
-      <div class="time">取消时间：2017-9-11 12：22：30<span class="succeed">已完成</span></div>
-      <div class="content">
-        <img src="@/assets/2.jpg" alt="" class="pic">
-         <div>
-          <div class="name">商品名称</div>
-          <div class="price">预估价格：<span>￥39.9</span></div>
-        </div>
-      </div>
-    </div>
     <div class="orders_wrap_text"><span class="line_lf"></span>没有更多订单了<span class="line_rt"></span></div>
   </div>
 </template>
 
 <script>
   import '@/components/details/orders.css'
-  export default {
+  import api from '@/api/api.js'
 
+  export default {
+    data() {
+      return {
+        ordersList:{},
+      }
+    },
+    mounted() {
+      //获取数据
+      api.getOrders().then((res) => {
+        console.log(res.data.data.listOrder);
+        res.data.data.listOrder.map(items => {
+          const status = items.statusPage;
+          switch (status) {
+            case '已接单':
+              items.statusClass= 'already';
+              break;
+            case '已派单':
+              items.statusClass= 'complete';
+              break;
+            case '待接单':
+              items.statusClass= 'waiting';
+              break;
+            case '已取消':
+              items.statusClass= 'cancel';
+              break;
+            case '已完成':
+              items.statusClass= 'succeed';
+              break;
+            default:
+              break;
+          }
+        });
+          this.ordersList = res.data.data.listOrder;
+      }).catch((erro) => {
+        console.log(erro)
+      })
+    }
   }
 </script>
